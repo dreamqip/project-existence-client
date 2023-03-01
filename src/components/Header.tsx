76b985bce72e22a989627d8cd9eaf0dfd60b3952
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-03-01 12:42:38 +0100

    Search
    For dekstop devices. Need to be checked

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from '@/styles/Header.module.scss';
 import Header__menu from './Header__menu';
 import Header__mobile_menu from './Header__mobile_menu';
-
-import { Button, Input, Title, ActionIcon } from '@mantine/core';
+import { ORGANISATION_FACTORY_ADDRESS } from '@/config';
+import {
+  Button,
+  TextInput,
+  Title,
+  ActionIcon,
+  Notification,
+} from '@mantine/core';
 import { Search, Wallet } from 'tabler-icons-react';
 import { getProvider } from '@/contract_interactions';
 import { NETWORK } from '@/config';
+import {
+  getOrganisationFactoryContract,
+  searchForOrganisationOrRegister,
+  getSigner,
+  OrganisationFactoryContract,
+} from '@/contract_interactions';
 
 const updateProvider = require('../contract_interactions').updateProvider as (
   chain: 'mainnet' | 'testnet' | 'fakenet',
@@ -16,6 +28,36 @@ const updateProvider = require('../contract_interactions').updateProvider as (
 
 export default function Header() {
   const [walletConnected, setWalletConnected] = useState(getProvider() != null);
+  const [contractAddr, setContractAddr] = useState({
+    address: '',
+  });
+  const [orgFactory, setOrgFactory] = useState<
+    OrganisationFactoryContract | undefined
+  >(undefined);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      if (isMounted) {
+        let signer = getSigner();
+        if (signer == null) return;
+
+        let factory = await getOrganisationFactoryContract(
+          ORGANISATION_FACTORY_ADDRESS,
+        );
+        if (factory != null) {
+          setOrgFactory(factory);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   return (
     <header className={styles.header}>
@@ -27,14 +69,33 @@ export default function Header() {
             </Title>
           </Link>
           <div className={styles.header__search}>
-            <ActionIcon className={styles.search__button}>
+            <ActionIcon
+              className={styles.search__button}
+              onClick={() =>
+                searchForOrganisationOrRegister(
+                  contractAddr.address,
+                  orgFactory,
+                )
+              }
+            >
               <Search />
             </ActionIcon>
-            <Input
+            <TextInput
               icon={<Search />}
               placeholder='Enter Org or Reg address'
               radius='md'
               size='md'
+              onChange={(event) =>
+                setContractAddr({ address: event.target.value })
+              }
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  searchForOrganisationOrRegister(
+                    contractAddr.address,
+                    orgFactory,
+                  );
+                }
+              }}
             />
           </div>
         </div>
