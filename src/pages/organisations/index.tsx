1725e2b7da6e75d59c90206f8cad384390d7b481
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-03-02 21:12:20 +0100

    Notification

--- src/pages/organisations/index.tsx
+++ src/pages/organisations/index.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import OrganisationCard from '@/components/Card';
-import { Title, Text, Breadcrumbs, Anchor } from '@mantine/core';
+import { Title, Text, Breadcrumbs, Anchor, Notification } from '@mantine/core';
 import styles from '@/styles/Organisations.module.scss';
 import Link from 'next/link';
-import { getOrganisationContract, getSigner, OrganisationContract } from '@/contract_interactions';
+import {
+  getOrganisationContract,
+  getSigner,
+  OrganisationContract,
+} from '@/contract_interactions';
 import { parseMetadata, waitFor } from '@/utils';
 import { FEATURED_ORGANISATIONS } from '@/config';
 
@@ -17,35 +21,61 @@ const items = [
 ));
 
 export default function organisations() {
-  const [featuredOrganisationsCards, setFeaturedOrganisationsCards] = useState([<React.Fragment key='1'>please connect your wallet</React.Fragment>] as JSX.Element[]);
-  
+  const [featuredOrganisationsCards, setFeaturedOrganisationsCards] = useState([
+    <React.Fragment key='1'>please connect your wallet</React.Fragment>,
+  ] as JSX.Element[]);
+
   useEffect(() => {
     let isMounted = true;
 
     const fetchData = async () => {
       if (isMounted) {
-        setFeaturedOrganisationsCards([<React.Fragment key='1'>loading...</React.Fragment>]);
+        setFeaturedOrganisationsCards([
+          <React.Fragment key='1'>
+            <Notification
+              withCloseButton={false}
+              color='yellow'
+              title='Loading...'
+            ></Notification>
+          </React.Fragment>,
+        ]);
 
         let orgs: OrganisationContract[] = [];
         for (const orgAddress of FEATURED_ORGANISATIONS) {
-          let org = await getOrganisationContract(orgAddress.toString())
+          let org = await getOrganisationContract(orgAddress.toString());
           if (org != null) orgs.push(org);
         }
-        
-        let orgCards = await Promise.all(orgs.map(async (org: OrganisationContract, index) => {
-          let metadata = parseMetadata(await org.metadata());
 
-          return <OrganisationCard
-            title={metadata.name ?? "name"}
-            key={index}
-            description={metadata.description ?? "description"}
-            badge='Featured'
-            contacts={metadata.contacts ?? "contacts"}
-            way={'/organisations/' + await org.getAddress()}
-          />
-        }));
+        let orgCards = await Promise.all(
+          orgs.map(async (org: OrganisationContract, index) => {
+            let metadata = parseMetadata(await org.metadata());
+
+            return (
+              <OrganisationCard
+                title={metadata.name ?? 'name'}
+                key={index}
+                description={metadata.description ?? 'description'}
+                badge='Featured'
+                contacts={metadata.contacts ?? 'contacts'}
+                way={'/organisations/' + (await org.getAddress())}
+              />
+            );
+          }),
+        );
 
         setFeaturedOrganisationsCards(orgCards);
+
+        if (orgs.length == 0) {
+          setFeaturedOrganisationsCards([
+            <React.Fragment key='1'>
+              <Notification
+                withCloseButton={false}
+                color='red'
+                title='Error'
+              ></Notification>
+            </React.Fragment>,
+          ]);
+        }
       }
     };
 
