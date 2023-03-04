35b78f7c3c39cd7f75ce42eebf1c46263c822623
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-03-04 20:14:01 +0100

    Activity styles compatibility

--- src/pages/organisations/[id].tsx
+++ src/pages/organisations/[id].tsx
@@ -338,7 +338,7 @@ export default function Organisation() {
               </Tabs.Panel>
 
               <Tabs.Panel value='activity' pt='xs'>
-                <Table highlightOnHover>
+                <Table highlightOnHover fontSize='md'>
                   <thead>
                     <tr>
                       <th>Register address</th>
