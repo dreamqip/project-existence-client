b6e13aaf435f4efdabe42c061df0b1bf8ea409ba
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-03-05 07:56:17 +0100

    Swap action and address

--- src/pages/organisations/[id]/[regAddr].tsx
+++ src/pages/organisations/[id]/[regAddr].tsx
@@ -38,12 +38,17 @@ import {
   getRegisterContract,
   type Record,
 } from '@/contract_interactions';
-import { parseMetadata, timestampToDate, toFunctionSelector, waitFor } from '@/utils';
+import {
+  parseMetadata,
+  timestampToDate,
+  toFunctionSelector,
+  waitFor,
+} from '@/utils';
 import { NULL_ADDR, NULL_HASH } from '@/config';
 import { ethers } from 'ethers';
 import { getActivityTransactions } from '@/tracer_interactions';
 
-export let update = () => { };
+export let update = () => {};
 
 export default function Register() {
   const router = useRouter();
@@ -146,8 +151,9 @@ export default function Register() {
 
   const rows = activityElements.map((element, index) => (
     <tr key={index}>
+      <td>{element.action}</td>
       <td>
-        {element.address == '' ? null :
+        {element.address == '' ? null : (
           <>
             <CopyButton value={element.address}>
               {({ copied, copy }) => (
@@ -164,9 +170,8 @@ export default function Register() {
             </CopyButton>
             {element.address}
           </>
-        }
+        )}
       </td>
-      <td>{element.action}</td>
       <td>{element.date}</td>
     </tr>
   ));
@@ -201,39 +206,45 @@ export default function Register() {
       );
     })();
     (async () => {
-      let txs = await getActivityTransactions(await reg.getAddress())
+      let txs = await getActivityTransactions(await reg.getAddress());
       if (txs.error != null) {
         setActivityElements([
           {
             address: 'Error.',
             date: 'Error.',
             action: 'Error.',
-          }
-        ])
+          },
+        ]);
       } else {
-        let transactions = txs.transactions
-        setActivityElements(transactions.sort((a, b) => a.timestamp > b.timestamp ? -1 : 1).map((item) => {
-          let action = item.functionSelector;
-          let address = '';
-          switch (item.functionSelector) {
-            case toFunctionSelector('createRecord(bytes32,string,string,uint256,uint256,bytes32)'):
-              action = 'Create Record';
-              address = "0x" + item.rawInput.slice(10, 74);
-              break;
-            case toFunctionSelector('invalidateRecord(bytes32)'):
-              action = 'Invalidate Record';
-              address = "0x" + item.rawInput.slice(10, 74);
-              break;
-            case toFunctionSelector('editRegisterMetadata(string)'):
-              action = 'Update Register';
-              break;
-          }
-          return {
-            address: address,
-            date: timestampToDate(item.timestamp * 1000, true),
-            action: action,
-          };
-        }));
+        let transactions = txs.transactions;
+        setActivityElements(
+          transactions
+            .sort((a, b) => (a.timestamp > b.timestamp ? -1 : 1))
+            .map((item) => {
+              let action = item.functionSelector;
+              let address = '';
+              switch (item.functionSelector) {
+                case toFunctionSelector(
+                  'createRecord(bytes32,string,string,uint256,uint256,bytes32)',
+                ):
+                  action = 'Create Record';
+                  address = '0x' + item.rawInput.slice(10, 74);
+                  break;
+                case toFunctionSelector('invalidateRecord(bytes32)'):
+                  action = 'Invalidate Record';
+                  address = '0x' + item.rawInput.slice(10, 74);
+                  break;
+                case toFunctionSelector('editRegisterMetadata(string)'):
+                  action = 'Update Register';
+                  break;
+              }
+              return {
+                address: address,
+                date: timestampToDate(item.timestamp * 1000, true),
+                action: action,
+              };
+            }),
+        );
       }
     })();
   };
@@ -351,7 +362,7 @@ export default function Register() {
                 <Notification
                   color={
                     Number(searchResult.expiresAt) == 0 ||
-                      Number(searchResult.expiresAt) > new Date().getTime()
+                    Number(searchResult.expiresAt) > new Date().getTime()
                       ? 'teal'
                       : 'red'
                   }
@@ -366,7 +377,7 @@ export default function Register() {
                     <Text
                       c={
                         Number(searchResult.expiresAt) == 0 ||
-                          Number(searchResult.expiresAt) > new Date().getTime()
+                        Number(searchResult.expiresAt) > new Date().getTime()
                           ? 'teal'
                           : 'red'
                       }
@@ -517,8 +528,8 @@ export default function Register() {
                             </Text>{' '}
                             {searchResult.createdAt.toString() != '0'
                               ? transformToDate(
-                                searchResult.createdAt.toString(),
-                              )
+                                  searchResult.createdAt.toString(),
+                                )
                               : '-'}
                           </List.Item>
                           <List.Item>
@@ -527,8 +538,8 @@ export default function Register() {
                             </Text>{' '}
                             {searchResult.updatedAt.toString() != '0'
                               ? transformToDate(
-                                searchResult.updatedAt.toString(),
-                              )
+                                  searchResult.updatedAt.toString(),
+                                )
                               : '-'}
                           </List.Item>
                           <List.Item>
@@ -537,8 +548,8 @@ export default function Register() {
                             </Text>
                             {searchResult.startsAt.toString() != '0'
                               ? transformToDate(
-                                searchResult.startsAt.toString(),
-                              )
+                                  searchResult.startsAt.toString(),
+                                )
                               : '-'}
                           </List.Item>
                           <List.Item>
@@ -547,8 +558,8 @@ export default function Register() {
                             </Text>
                             {searchResult.expiresAt.toString() != '0'
                               ? transformToDate(
-                                searchResult.expiresAt.toString(),
-                              )
+                                  searchResult.expiresAt.toString(),
+                                )
                               : '-'}
                           </List.Item>
                           <List.Item sx={{ overflowWrap: 'break-word' }}>
@@ -621,8 +632,8 @@ export default function Register() {
                 <Table highlightOnHover fontSize='md'>
                   <thead>
                     <tr>
-                      <th className={styles.column}>Document hash</th>
                       <th className={styles.column}>Action</th>
+                      <th className={styles.column}>Document hash</th>
                       <th className={styles.column}>When</th>
                     </tr>
                   </thead>
